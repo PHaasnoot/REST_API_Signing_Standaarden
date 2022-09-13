b52811b2e5e0c8f2ce404cda5e5082dd12d43186
PHaasnoot <50872565+PHaasnoot@users.noreply.github.com>
2022-09-13 11:29:09 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
 var respecConfig =
 {
   //voor specStatus mogelijkheden zie https://github.com/Logius-standaarden/respec/wiki/specStatus
-  specStatus: "WV",
+  specStatus: "-",
   //voor specType mogelijkheden zie https://github.com/Logius-standaarden/respec/wiki/specType
-  specType: "HR",
+  specType: "-",
   pubDomain: "dk",
   shortName: "template",
   publishDate: "2022-06-24",
